ffe4e89a2f77690221091bdd7fce67015c959c3c
badsha <badshanoordeen@gmail.com>
2025-09-27 21:02:08 +0530

    https cors

--- dist/src/main.js
+++ dist/src/main.js
@@ -8,7 +8,10 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
     app.enableCors({
-        origin: [process.env.FRONTEND_URL, 'front-commerce-rust.vercel.app'],
+        origin: [
+            process.env.FRONTEND_URL,
+            'https://front-commerce-rust.vercel.app',
+        ],
         credentials: true,
     });
     await app.listen((_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3001, '0.0.0.0');
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, 'front-commerce-rust.vercel.app'],
+    origin: [
+      process.env.FRONTEND_URL,
+      'https://front-commerce-rust.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0'); // 👈 important
